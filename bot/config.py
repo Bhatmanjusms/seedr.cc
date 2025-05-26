21929d0d461bc74c44967616f804dcd5b4a53dde
Manjunath Bhat <86521081+Bhatmanjusms@users.noreply.github.com>
2025-05-26 13:51:12 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,4 +5,4 @@ load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("7654936998:AAHm5adLkRMDBvx_KK3OluGDo_PBDS8SH9g")
 SEEDR_USERNAME = os.getenv("btmanjusms@gmail.com")
-SEEDR_PASSWORD = os.getenv("sindhoora")
+SEEDR_PASSWORD = os.getenv("Manjunath@123")
