03b5b0a7824228285c7a442cd1045259560becfb
Manjunath Bhat <86521081+Bhatmanjusms@users.noreply.github.com>
2025-05-26 14:11:45 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,6 +3,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_TOKEN = os.getenv("7654936998:AAHm5adLkRMDBvx_KK3OluGDo_PBDS8SH9g")
-SEEDR_USERNAME = os.getenv("btmanjusms@gmail.com")
-SEEDR_PASSWORD = os.getenv("Manjunath@123")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
